47726a0f3e79ad84b12e332fbc786f271a796062
Cezary Głodek <cezariusz.g@gmail.com>
2023-02-23 00:45:35 +0100

    added new js functions, added new image.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-console.log("Hello!")
 
-let header = document.querySelector(".header")
-header.addEventListener("click", () => {
-    header.remove();
-})
+{
+    const welcome = () => {
+        console.log("Welcome to all developers!");
+    }
+    welcome();
+
+    const changePortrait = () => {
+        const portrait = document.querySelector(".js-portrait")
+        const portraitPrev = document.querySelector(".js-portraitPrev");
+        const portraitNext = document.querySelector(".js-portraitNext");
+        buttonPrev.addEventListener("click", () => {
+            // body.classList.toggle("body--light");
+
+            // body.classList.contains("body--light") ? button.innerText = "Przyciemnij tło" : button.innerText = "Rozjaśnij tło";
+        });
+    }
+    changePortrait();
+}
