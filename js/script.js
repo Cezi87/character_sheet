503f7ffa9d6384f038a9280971f8504437811b2c
Cezary Głodek <cezariusz.g@gmail.com>
2023-03-06 14:16:35 +0100

    changed stat button css

--- js/script.js
+++ js/script.js
@@ -4,24 +4,28 @@
     }
     welcome();
 
-    const select = document.querySelector('.js-charClass');
-    const portrait = document.querySelector('.js-portrait');
+    const portrait = () => {
 
-    select.addEventListener('change', () => {
-        const selectedPortrait = select.value;
-        switch (selectedPortrait) {
-            case 'Warrior':
-                portrait.className = 'js-portrait section__portraitWarrior';
-                break;
-            case 'Bandit':
-                portrait.className = 'js-portrait section__portraitBandit';
-                break;
-            case 'Knight':
-                portrait.className = 'js-portrait section__portraitKnight';
-                break;
-            default:
-                portrait.className = 'js-portrait section__portraitWarrriorr';
-                break;
-        }
-    });
+        const select = document.querySelector('.js-charClass');
+        const portrait = document.querySelector('.js-portrait');
+
+        select.addEventListener('change', () => {
+            const selectedPortrait = select.value;
+            switch (selectedPortrait) {
+                case 'Warrior':
+                    portrait.className = 'js-portrait section__portraitWarrior';
+                    break;
+                case 'Bandit':
+                    portrait.className = 'js-portrait section__portraitBandit';
+                    break;
+                case 'Knight':
+                    portrait.className = 'js-portrait section__portraitKnight';
+                    break;
+                default:
+                    portrait.className = 'js-portrait section__portraitWarrior';
+                    break;
+            }
+        });
+    }
+    portrait();
 }
