81ef35b85e51e1f671662eee52cb4ca4af0814bc
Cezary Głodek <cezariusz.g@gmail.com>
2023-03-06 11:41:49 +0100

    changed portrait to switch with class selection.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-
 {
     const welcome = () => {
         console.log("Welcome to all developers!");
     }
     welcome();
 
-    const changePortrait = () => {
-        const portrait = document.querySelector(".js-portrait")
-        const portraitPrev = document.querySelector(".js-portraitPrev");
-        const portraitNext = document.querySelector(".js-portraitNext");
-        portraitNext.addEventListener("click", () => {
-            portrait.classList.add("section__portrait2");
-        });
-        portraitPrev.addEventListener("click", () => {
-            portrait.classList.remove("section__portrait2");
-        });
-    }
-    changePortrait();
+    const select = document.querySelector('.js-charClass');
+    const portrait = document.querySelector('.js-portrait');
+
+    select.addEventListener('change', () => {
+        const selectedPortrait = select.value;
+        switch (selectedPortrait) {
+            case 'Warrior':
+                portrait.className = 'js-portrait section__portraitWarrior';
+                break;
+            case 'Bandit':
+                portrait.className = 'js-portrait section__portraitBandit';
+                break;
+            case 'Knight':
+                portrait.className = 'js-portrait section__portraitKnight';
+                break;
+            default:
+                portrait.className = 'js-portrait section__portraitWarrriorr';
+                break;
+        }
+    });
 }
